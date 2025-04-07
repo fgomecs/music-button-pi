f13a8998ae7af82f0fc6ddebbcf9bd8ee01e9ccd
Francisco G <fgomecs@icloud.com>
2025-04-07 17:16:12 -0400

    Added song skillping script

--- play_song.py
+++ play_song.py
@@ -5,18 +5,39 @@ import time
 GPIO.setmode(GPIO.BCM)
 BUTTON_PIN = 18
 GPIO.setup(BUTTON_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-SONG_PATH = "/home/tiago/music-button-pi/song.mp3"
 
-print("Press the button to play the song (Ctrl+C to exit)...")
+SONGS = [
+    "/home/tiago/music-button-pi/song.mp3",
+    "/home/tiago/music-button-pi/song2.mp3",
+    "/home/tiago/music-button-pi/song3.mp3",
+    "/home/tiago/music-button-pi/song4.mp3",
+    "/home/tiago/music-button-pi/song5.mp3"
+]
+current_song_index = 0
+current_process = None
+last_button_state = GPIO.LOW
+
+print("Press the button to play or skip to the next song (Ctrl+C to exit)...")
 try:
     while True:
-        if GPIO.input(BUTTON_PIN) == GPIO.HIGH:
-            print("Button pressed! Playing song...")
-            subprocess.run(["mpv", "--no-video", SONG_PATH])
-            print("Song finished.")
-            time.sleep(0.2)
+        button_state = GPIO.input(BUTTON_PIN)
+        if button_state == GPIO.HIGH and last_button_state == GPIO.LOW:
+            # Kill the current song if playing
+            if current_process is not None:
+                current_process.terminate()
+                current_process.wait()  # Ensure it’s dead
+            # Move to next song
+            song_path = SONGS[current_song_index]
+            print(f"Playing song {current_song_index + 1}: {song_path}")
+            current_process = subprocess.Popen(["mpv", "--volume=150", song_path])
+            current_song_index = (current_song_index + 1) % len(SONGS)
+            time.sleep(0.5)  # Debounce
+        last_button_state = button_state
         time.sleep(0.1)
 except KeyboardInterrupt:
     print("Exiting...")
+    if current_process is not None:
+        current_process.terminate()
 finally:
     GPIO.cleanup()
+    subprocess.run(["pkill", "-9", "mpv"])  # Final cleanup
