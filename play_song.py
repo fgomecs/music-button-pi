3d762202ee7abc021c6d3fac54f6023c80b91a9e
Francisco G <fgomecs@icloud.com>
2025-04-07 11:40:48 -0400

    Initial commit: Added music button scripts and README

--- play_song.py
+++ play_song.py
@@ -2,23 +2,19 @@ import RPi.GPIO as GPIO
 import subprocess
 import time
 
-# Set up GPIO
 GPIO.setmode(GPIO.BCM)
 BUTTON_PIN = 18
 GPIO.setup(BUTTON_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-
-# URL of the song
-SONG_URL = "https://www.youtube.com/watch?v=l82wUVuzegY"
+SONG_PATH = "/home/tiago/music-button-pi/song.mp3"
 
 print("Press the button to play the song (Ctrl+C to exit)...")
-
 try:
     while True:
         if GPIO.input(BUTTON_PIN) == GPIO.HIGH:
             print("Button pressed! Playing song...")
-            subprocess.run(["mpv", "--no-video", SONG_URL])
+            subprocess.run(["mpv", "--no-video", SONG_PATH])
             print("Song finished.")
-            time.sleep(0.2)  # Debounce
+            time.sleep(0.2)
         time.sleep(0.1)
 except KeyboardInterrupt:
     print("Exiting...")
